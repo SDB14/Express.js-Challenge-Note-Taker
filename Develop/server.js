953d13610a4841d7614798035306aa68d67e3f84
SDB14 <cwruboot22@gmail.com>
2021-11-09 17:44:44 -0500

    add files

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+// Initialize the app and create a port
+const express = require("express");
+const app =express()
+const htmlroutes=require("./routing/htmlroutes")
+
+
+const PORT = process.env.PORT || 3001;
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use("/",htmlroutes)
+
+
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
